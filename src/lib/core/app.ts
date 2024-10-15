1e974389e9b6851c79ee0391391ee4ad644b5ccd
Rory Duncan <robust.rory@gmail.com>
2024-10-14 21:37:05 -0700

    Only allow window access via csr

--- src/lib/core/app.ts
+++ src/lib/core/app.ts
@@ -3,23 +3,26 @@ import { Application, Sprite, Assets, Container } from "pixi.js";
 const PIXI_INIT_EVENT = "pixi_initialized" as const;
 
 const is_ready_promise = new Promise<void>((r) => {
-  window.addEventListener(
-    PIXI_INIT_EVENT,
-    () => {
-      console.log("Pixi was initialized");
-      r();
-    },
-    {
-      once: true,
-    },
-  );
+  if (typeof window !== "undefined") {
+    window.addEventListener(
+      PIXI_INIT_EVENT,
+      () => {
+        console.log("Pixi was initialized");
+        r();
+      },
+      {
+        once: true,
+      },
+    );
+  }
 });
 
 export const app = new Application();
-console.log("APP", app);
 
 export const signal_as_ready = () => {
-  window.dispatchEvent(new CustomEvent(PIXI_INIT_EVENT));
+  if (typeof window !== "undefined") {
+    window.dispatchEvent(new CustomEvent(PIXI_INIT_EVENT));
+  }
 };
 
 export const is_ready = () => is_ready_promise;
