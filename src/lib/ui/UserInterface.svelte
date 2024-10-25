c53f0cd04389dddcdf5925cb8df22b3ddf3311ce
Rory Duncan <robust.rory@gmail.com>
2024-10-24 22:48:04 -0700

    Allow pointer events to ui elements but not to the ui-root

--- src/lib/ui/UserInterface.svelte
+++ src/lib/ui/UserInterface.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
   import { root_el } from "./ui.js";
-
-  // let component: ComponentType | undefined = undefined;
-  // let state: Readable<any> | undefined = undefined;
-  // let events: EventDispatcher<Record<string, any>> | undefined = undefined;
-
-  // $: events = $current_ui?.events;
-  // $: console.log("current_ui, from UserInterface.svelte:", $current_ui);
 </script>
 
 <main class="stack">
@@ -15,8 +8,7 @@
   </div>
 
   <div class="ui-root roboto-mono">
-    <!-- <svelte:component this={component} {state} {events} /> -->
-    <div bind:this={$root_el} />
+    <div bind:this={$root_el} class="ui-interactive" />
     <slot name="ui" />
   </div>
 </main>
@@ -37,6 +29,10 @@
   .ui-root {
     padding: 1rem 1rem;
     z-index: 2;
+    pointer-events: none;
+  }
+  .ui-interactive {
+    pointer-events: all;
   }
   .roboto-mono {
     font-family: "Roboto Mono", monospace;
